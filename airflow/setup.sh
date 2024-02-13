801c1c66afa1d27e7f4c0c5d0a6318b12c5c1923
uncle-alfer <70284100+uncle-alfer@users.noreply.github.com>
2024-02-13 22:04:25 +0300

    Update setup.sh

--- airflow/setup.sh
+++ airflow/setup.sh
@@ -6,7 +6,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 apt-cache policy docker-ce
 
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 # sudo usermod -aG docker ${USER}
 # su - ${USER}
 
