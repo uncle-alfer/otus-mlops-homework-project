06139a1d41ff9a224e9891013a3ad43ab5b70462
uncle-alfer <70284100+uncle-alfer@users.noreply.github.com>
2024-02-13 22:16:00 +0300

    Create cleanse_data_dag1.py

---
+++ airflow/dags/cleanse_data_dag1.py
@@ -0,0 +1,58 @@
+import airflow
+from datetime import timedelta
+from airflow import DAG
+# from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.sftp.operators import sftp_operato
+from airflow.contrib.hooks import SSHHook
+from airflow.providers.ssh.operators.ssh import SSHOperator
+# from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+# sshHook = SSHHook(conn_id="ssh_default", key_file="/home/airflow/keys/ssh.key")
+sshHook = SSHHook(remote_host='', username='worker', key_file='/home/airflow/keys/ssh.key')
+
+default_args = {
+    'owner': 'airflow',
+    #'start_date': airflow.utils.dates.days_ago(2),
+    # 'end_date': datetime(),
+    # 'depends_on_past': False,
+    # 'email': ['airflow@example.com'],
+    # 'email_on_failure': False,
+    #'email_on_retry': False,
+    # If a task fails, retry it once after waiting
+    # at least 5 minutes
+    #'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag_spark = DAG(
+    dag_id = "sparkoperator",
+    default_args=default_args,
+    # schedule_interval='0 0 * * *',
+    schedule_interval='@once',
+    dagrun_timeout=timedelta(minutes=10),
+    description='use case of sparkoperator in airflow',
+    start_date = airflow.utils.dates.days_ago(1)
+)
+
+print("send_file_to_otjer_vm")
+send_file = SFTPOperator(
+    task_id="send_file",
+    ssh_conn_id="ssh_default",
+    local_filepath="/home/airflow/utils/cleanse_data.py",
+    remote_filepath="/home/worker/utils/cleanse_data.py",
+    operation="put",
+    dag=dag_spark
+)
+
+run_cmd = (
+    task_id="run_cmd",
+    command="touch FOOOOCK.txt",
+    ssh_hook=sshHook,
+    dag=dag_spark,
+)
+
+send_file >> run_cmd
+
+if __name__ == "__main__":
+    dag_spark.cli()
