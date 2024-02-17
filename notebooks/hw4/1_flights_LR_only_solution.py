eabba4a19979a7a072fb1600626d33d1e9b6d3be
uncle-alfer <70284100+uncle-alfer@users.noreply.github.com>
2024-02-18 00:58:03 +0300

    Update 1_flights_LR_only_solution.py

--- notebooks/hw4/1_flights_LR_only_solution.py
+++ notebooks/hw4/1_flights_LR_only_solution.py
@@ -28,23 +28,21 @@ def get_regression():
 
 
 def main(args):
-
+    os.environ["AWS_ACCESS_KEY_ID"] = ...
+    os.environ["AWS_SECRET_ACCESS_KEY"] = ...
     os.environ["MLFLOW_S3_ENDPOINT_URL"] = "https://storage.yandexcloud.net"
-    TRACKING_SERVER_HOST = '127.0.0.1'
-    mlflow.set_tracking_uri(TRACKING_SERVER_HOST)
-    # mlflow.set_registry_uri(TRACKING_SERVER_HOST)
 
-    logger.info("tracking URI: %s", {mlflow.get_tracking_uri()})
+    logger.info("========================tracking URI: %s", {mlflow.get_tracking_uri()})
 
     mlflow.set_experiment("pyspark_experiment")
 
-    logger.info("Creating Spark Session ...")
+    logger.info("========================Creating Spark Session ...")
     spark = SparkSession\
         .builder\
         .appName("pyspark_experiment_1")\
         .getOrCreate()
 
-    logger.info("Loading Data ...")
+    logger.info("========================Loading Data ...")
     data = spark.createDataFrame(get_dataframe())
     
     assembler = VectorAssembler(inputCols=["age", "bmi"], outputCol="features")
@@ -54,9 +52,9 @@ def main(args):
 
     model = regression.fit(train_data)
 
-    logger.info("Saving model ...")
+    logger.info("========================Saving model ...")
+    mlflow.spark.log_model(model, artifact_path=args.output_artifact)
     mlflow.spark.save_model(model, args.output_artifact)
-    # how to log to mlflow?
 
     spark.stop()
 
@@ -66,7 +64,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Model (Inference Pipeline) Training")
 
-    # При запуске используйте оригинальное имя 'Student_Name_flights_LR_only'
     parser.add_argument(
         "--output_artifact",
         type=str,
