11d767ba4ab03811392bfa4b513b6e55795ae543
uncle-alfer <70284100+uncle-alfer@users.noreply.github.com>
2024-02-18 00:58:20 +0300

    Update 1_flights_LR_only_solution.sh

--- notebooks/hw4/1_flights_LR_only_solution.sh
+++ notebooks/hw4/1_flights_LR_only_solution.sh
@@ -1,3 +1,6 @@
+export MLFLOW_TRACKING_URI="http://51.250.64.9:8000"
+export MLFLOW_S3_ENDPOINT_URL="https://mlops-hw3-vos.website.yandexcloud.net"
+
 spark-submit \
 --jars mlflow-spark-1.27.0.jar \
 1_flights_LR_only_solution.py \
