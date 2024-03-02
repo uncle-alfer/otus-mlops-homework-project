a9151b5e73b589eae0e5c0e3f0d5ab709766f8b6
uncle-alfer <vlad.sheremetiev@gmail.com>
2024-03-02 23:01:36 +0000

    predict

--- project/src/predict.py
+++ project/src/predict.py
@@ -1,19 +1,38 @@
-# import logging
-# import os
-# from datetime import datetime
+import logging
+import os
+from io import BytesIO
 
-# import numpy as np
-# import pandas as pd
-# import xgboost as xgb
-# from mlflow.tracking import MlflowClient
-# from sklearn.compose import ColumnTransformer
-# from sklearn.impute import SimpleImputer
-# from sklearn.metrics import accuracy_score
-# from sklearn.model_selection import GridSearchCV, train_test_split
-# from sklearn.pipeline import Pipeline
-# from sklearn.preprocessing import OneHotEncoder
+import boto3
+import pandas as pd
+from mlflow.tracking import MlflowClient
 
-# import mlflow
+import mlflow
 
-# logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
-# logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+os.environ["MLFLOW_TRACKING_URI"] = "http://158.160.97.90:8000"
+client = MlflowClient()
+model_version = client.get_model_version_by_alias(name="inf1", alias="champion")
+
+model = mlflow.pyfunc.load_model(model_version.source)
+
+bucket_name = "project-mlops-object-storage"
+
+
+def read_raw_data():
+    s3 = boto3.resource(service_name="s3", endpoint_url="https://storage.yandexcloud.net")
+    bucket = s3.Bucket(bucket_name)
+    dfs = []
+    for obj in bucket.objects.filter(Prefix="raw_data/train"):
+        # key = obj.key
+        body = obj.get()["Body"].read()
+        df = pd.read_csv(BytesIO(body), encoding="utf8")
+        dfs.append(df)
+    df = pd.concat(dfs)
+    return df
+
+
+df = read_raw_data()
+
+print(model.predict(df))
+# print(model)
